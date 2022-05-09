102e49516ae6d6f7cd9bf4cb19e3643dcd8093f7
keshav <kdooleea@yahoo.ca>
2022-05-08 22:17:28 -0400

    updated deployed prod server url

--- classes/utils/config.ts
+++ classes/utils/config.ts
@@ -1,5 +1,5 @@
 export const isLocal = process.env.NODE_ENV !== "production";
-export const RKD_SERVER_URL = isLocal ? "http://localhost:8080" : "https://www.rkdooleea.com/api";
+export const RKD_SERVER_URL = isLocal ? "http://localhost:8080/api" : "http://rkd-portfolio-backend.herokuapp.com/api";
 
 export const APP_NAME = "RKD_CHAT_APP";
 
